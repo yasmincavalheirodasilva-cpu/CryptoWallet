217f4659e1363fd2a0f6d2a0b0023dc8fbbabf65
yasmincavalheirodasilva-cpu <yasmincavalheirodasilva@gmail.com>
2026-06-03 20:49:06 -0300

    Update Carteira.java

--- Carteira.java
+++ Carteira.java
@@ -9,6 +9,6 @@ public class Carteira {
 
     public void depositar(double qtd) {
         this.saldoBitcoin += qtd;
-        System.out.println("Déposito feitooo!");
+        System.out.println("Déposito feito!");
     }
 }
